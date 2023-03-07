9b3976b5b42511c8ad2fe0515a17190ed3371550
maejo-o <efforterjisulee@gmail.com>
2023-03-07 15:31:52 +0900

    Initial Commit

---
+++ cin_cout_ex/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cin_cout_ex
+//
+//  Created by 이지수 on 2023/03/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
